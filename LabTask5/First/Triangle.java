0142e8f2c7878e6c6e31aa17e81126935e98665d
hanzlaahmadcheema <hanzlaahmdcheema@gmail.com>
2024-10-30 12:24:39 +0500

    h

--- LabTask5/First/Triangle.java
+++ LabTask5/First/Triangle.java
@@ -13,7 +13,6 @@ public class Triangle extends Shape {
 
     @Override
     public double calculateArea() {
-        // Using Heron's formula
         double s = (side1 + side2 + side3) / 2;
         return Math.sqrt(s * (s - side1) * (s - side2) * (s - side3));
     }
